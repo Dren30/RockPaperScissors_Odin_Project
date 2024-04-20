3e345559fb2c51b2e977d18beed5dbbd2c924a38
Schavitt <schavittu.a30@gmail.com>
2024-04-20 19:38:37 -0400

    create function that ask and save player's choice

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
+const prompt = require("prompt-sync")();
 
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * (3 - 1 + 1) ) + 1;
-    
     
     if (choice == 1) {
         return "Rock"
@@ -14,4 +13,37 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+
+function rockPaperScissors(computerChoice){
+    let wrongChoice = true; 
+    console.log('Make a choice (use the word): "rock" | "paper" | "scissors"');
+    let playerSelection;
+    
+    
+    while (wrongChoice) {
+
+        playerSelection = prompt("-->").toLowerCase();
+        
+        if (playerSelection !== ""){
+        
+            if ( playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+                console.log('\nChoose a valid answer! Use the word "rock" | "paper" | "scissors"\n');
+                
+            }else{
+
+                wrongChoice = false;           
+                
+            }
+
+        }else{
+
+            console.log('\nEmpty choice not acceptable. \nChoose a valid answer! Use the word "rock" | "paper" | "scissors"\n');
+        }
+        
+
+    }
+    
+}
+
+rockPaperScissors(getComputerChoice());
+
